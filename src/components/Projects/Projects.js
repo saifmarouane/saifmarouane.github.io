057dd9afa590f32a0fc27bdf564a3a1ace4706e5
saifmarouane <marouansaif3@gmial.com>
2023-10-27 22:32:38 +0100

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,10 +25,16 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Descrebo art"
+              description=" 
+              📱 Have you ever wanted to turn your imagination into reality? With this innovative app, I've combined the power of AI models like DALL-E and GPT to create a unique experience where you can bring your ideas to life through images.
+🤖 How does it work? Imagine chatting with a conversational assistant that guides you to describe an image. As the conversation evolves, the assistant intuitively prompts you to provide more details. When you're ready, simply press the generate button, and watch as the AI takes your descriptions and creates stunning images that match your vision.
+🎉 What sets this app apart:
+Seamless user interaction: The assistant engages you in a dynamic conversation, ensuring all details are captured.
+AI synergy: DALL-E and GPT work in harmony to understand your descriptions and turn them into vivid images
+              "
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -36,10 +42,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Chatbot Médical"
+              description="Le Chatbot Médical en JavaFX est une application interactive conçue pour offrir des informations médicales de base, des conseils de santé, et des réponses à des questions courantes sur la santé. Il vise à fournir un accès rapide à des informations médicales fiables ."
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -47,10 +53,11 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Powered App Comarkdownntent Generator with GPT"
+              description="This innovative app is designed to simplify content creation by guiding users through a conversation with our AI assistant. Whether you're crafting a YouTube video script, a blog post, or any other content, our app is here to assist you every step of the way. Just tell the assistant what you need to create, and it'll ask you the right questions to help you generate compelling content.
+              What inspired me to create this app? I saw an opportunity to provide a smarter, more user-friendly solution in the content creation space. While traditional chatbots may face challenges in this domain, our app aims to make the process seamless and creative"
+              ghLink="#"
+              demoLink="#"              
             />
           </Col>
 
@@ -59,9 +66,9 @@ function Projects() {
               imgPath={leaf}
               isBlog={false}
               title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              description="L'Application de Recommandation de Livres est une solution technologique innovante conçue en utilisant Flask, une application web Python, ainsi que des techniques de Traitement du Langage Naturel (NLP) et de l'apprentissage automatique (machine learning). Cette application vise à améliorer l'expérience de lecture en aidant les utilisateurs à découvrir des livres qui correspondent à leurs préférences et à leurs intérêts littéraires"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -69,9 +76,9 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Prompt engineering for Auto Assessment"
+              description=" Parmi les cas d'usage des technologies type Generative AI, le déploiement des assistants pour des tâches spécialisées.  Cette customisation passe par la conception d'un prompt dédié pour la tâche souhaitée et également par l'intégration d'autres couches et de fine tuning spécifique au contexte du projet. Afin d'automatiser l'auto-évaluation des candidats dans un parcours d'apprentissage, l'objectif du projet est de mettre en place une chaîne de chatbots prompts pour évaluer le niveau d'un candidat sur une technologie donnée en utilisant Chatgpt."
+              ghLink="#"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -80,10 +87,10 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              title=" Emotion Detection"
+              description="The Emotion Recognition Mobile App is a cutting-edge application developed using Java and TensorFlow within the Android Studio environment. This app leverages the power of machine learning to recognize and analyze human emotions based on input from various sources, such as images or audio recordingsTrained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              ghLink="#"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
